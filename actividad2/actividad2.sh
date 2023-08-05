c8f88e06655bceaeb2b8aa3d3ac1c4e4477e8852
Bvhdc <3005706780101@ingenieria.usac.edu.gt>
2023-08-05 11:04:05 -0600

    update on date

--- actividad2/actividad2.sh
+++ actividad2/actividad2.sh
@@ -1,4 +1,9 @@
 gitv="$1"
 url="https://api.github.com/users/"
 cons="${url} ${gitv}"
-curl -s $cons | jq '.' 
+content=$(curl -s $cons)
+userid=$( jq -r  '.id' <<< "${content}" )
+created=$( jq -r  '.created_at' <<< "${content}" )  
+now=$(date)
+log="/tmp/$(now)/saludos.log"
+echo $log
