b2d7289de1a566998f2ff4962c9f01fd84b50849
Bvhdc <40047058+Bvhdc@users.noreply.github.com>
2023-08-05 11:29:14 -0600

    done actividad2

--- actividad2/actividad2.sh
+++ actividad2/actividad2.sh
@@ -4,7 +4,9 @@ cons="${url}${gitv}"
 content=$(curl -s $cons)
 userid=$( jq -r  '.id' <<< "${content}" )
 created=$( jq -r  '.created_at' <<< "${content}" )  
-now=$(date)
+now=$(date "+%F-%T")
+dir="/tmp/$now"
 log="/tmp/$now/saludos.log"
+mkdir -p $dir
 touch $log
 echo “Hola $gitv. User ID: $userid. Cuenta fue creada el: $created.”>$log
