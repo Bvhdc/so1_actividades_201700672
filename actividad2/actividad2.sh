1d3a0d17fb6f5cee144eaf1bfbe83cd22ab60bd3
Bvhdc <3005706780101@ingenieria.usac.edu.gt>
2023-08-05 11:16:07 -0600

    first attempt on writting

--- actividad2/actividad2.sh
+++ actividad2/actividad2.sh
@@ -1,9 +1,10 @@
 gitv="$1"
 url="https://api.github.com/users/"
-cons="${url} ${gitv}"
+cons="${url}${gitv}"
 content=$(curl -s $cons)
 userid=$( jq -r  '.id' <<< "${content}" )
 created=$( jq -r  '.created_at' <<< "${content}" )  
 now=$(date)
-log="/tmp/$(now)/saludos.log"
-echo $log
+log="/tmp/$now/saludos.log"
+touch $log
+echo “Hola $gitv. User ID: $userid. Cuenta fue creada el: $created.”>$log
